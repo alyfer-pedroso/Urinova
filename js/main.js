60a6398be0f7c9f50a4d2fc672d0a995059a2575
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2023-11-13 22:01:36 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+$aboutUsBtnsS = document.querySelectorAll(".about-us_container");
+
+$aboutUsBtnsS.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        if (e.target.classList.contains("about-us_btn")) {
+            e.target.parentNode.classList.toggle("opened_about-us");
+        } else if (e.target.parentNode.classList.contains("about-us_btn")) {
+            e.target.parentNode.parentNode.classList.toggle("opened_about-us");
+        } else if (e.target.parentNode.parentNode.classList.contains("about-us_btn")) {
+            e.target.parentNode.parentNode.parentNode.classList.toggle("opened_about-us");
+        }
+    });
+});
+
 function headerFixed() {
     const $header = document.querySelector(".header");
     const $pixTop = document.querySelector("#pix");
